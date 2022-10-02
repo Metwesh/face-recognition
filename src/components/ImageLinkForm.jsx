b2aab8d596202881af4303531a5259b7336775ad
Metwesh <mohamedh.aly@hotmail.com>
2022-10-02 12:49:01 +0200

    Fixed link validation

--- src/components/ImageLinkForm.jsx
+++ src/components/ImageLinkForm.jsx
@@ -20,6 +20,8 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
               type="url"
               placeholder="Image link"
               autoComplete="off"
+              pattern="https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+              title="Link should start with HTTP/S"
               onChange={onInputChange}
               required
             />
